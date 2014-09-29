c423b179543b4a2df5784ff6a1b87ee197248ca7
Drew Jaqua <djaqua@smnet.net>
2014-09-28 23:32:38 -0400

    Removed obsolete code, changed permissions a little.

--- seam/seam-forms-lib.pl
+++ seam/seam-forms-lib.pl
@@ -3,54 +3,6 @@
 require "seam-ui-lib.pl";
 require "pjsmanager.pl";
 
-=head2 seam_domain_select_form(form_action, form_id, default_domain)
-=cut
-sub seam_domain_select_form {
-
-    local $form_action = get_param( $_[0], "select_user.cgi" );
-    local $form_id = get_param( $_[1], "domainSelectForm" );
-    local $default_domain = get_param( $_[2] );
-    local $domain_field_id = get_param( $_[3], "domain" );
-    
-    pjsmanager::valueChangeAutoSubmit( $domain_field_id, $form_id );
-
-    local $str_value;
-    $str_value .= ui_form_start($form_action, "POST", undef, "id=\"$form_id\"");
-
-    # its okay if $default_domain_id is the empty string or undefined
-    $str_value .= seam_domain_selector( $default_domain, $domain_field_id );
-
-    $str_value .= qq~<input type="submit" value="Update Domain">~;
-
-    $str_value .= ui_form_end();
-    return $str_value;
-}
-
-=head2 seam_domain_select_form(domain, form_action, form_id, default_user, user_field_id)
-    
-=cut
-sub seam_user_select_form {
-
-    local $domain = get_param( $_[0] );
-    local $form_action = get_param( $_[1], "choose_action.cgi" );
-    local $form_id = get_param( $_[2], "userSelectForm" );
-    local $default_user = get_param( $_[3] );
-    local $user_field_id = get_param( $_[4], "user" );
-
-    pjsmanager::valueChangeAutoSubmit( $user_field_id, $form_id );
-    
-    local $str_value;                                                   
-    $str_value .= ui_form_start($form_action, "POST", undef, "id=\"$form_id\"");
-    $str_value .= seam_user_selector( $domain, $default_user );                                                  
-    $str_value .= qq~                                                                       
-    <input type="hidden" name="domain" value="$domain">
-    <input type="submit" value="Update User">
-    ~;
-    $str_value .= ui_form_end();
-
-    return $str_value;
-
-}
 
 sub seam_add_forwarder_form {
 
